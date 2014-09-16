287244a9ad573d1e4c0edebee5de711f8aa32014
cheewing <cheewing@cheewingdembp.lan>
2014-09-16 12:21:44 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by cheewing on 14-9-16.
+//  Copyright (c) 2014年 cheewing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
